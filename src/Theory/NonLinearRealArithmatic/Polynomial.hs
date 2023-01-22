3542ec476bf7b4ff10a93b1eb5eca61c8da4e74c
Niklas Gruhn <niklas@gruhn.me>
2023-01-22 22:22:54 +0100

    tinker: Subtropical

--- src/Theory/NonLinearRealArithmatic/Polynomial.hs
+++ src/Theory/NonLinearRealArithmatic/Polynomial.hs
@@ -1,15 +1,17 @@
 module Theory.NonLinearRealArithmatic.Polynomial where
 
 import Theory.NonLinearRealArithmatic.Expr (Expr(..), Var, BinaryOp(..), UnaryOp(..))
+import qualified Data.Vector as V
 import qualified Data.List as L
+import qualified Data.IntMap as M
+import qualified Data.IntSet as S
+import Data.IntMap ( IntMap )
 import Data.Function ( on )
 
--- | List of variables paired with their positive integer exponents.
-type Monomial = [(Var, Int)]
+-- | Map of variables to integer exponents.
+type Monomial = IntMap Int
 
 data Term a = Term { getCoeff :: a, getMonomial :: Monomial }
-
-type Polynomial a = [Term a]
 
 modifyCoeff :: (a -> a) -> Term a -> Term a
 modifyCoeff f (Term coeff monomial) = Term (f coeff) monomial
@@ -17,9 +19,51 @@ modifyCoeff f (Term coeff monomial) = Term (f coeff) monomial
 modifyMonomial :: (Monomial -> Monomial) -> Term a -> Term a
 modifyMonomial f (Term coeff monomial) = Term coeff (f monomial)
 
+
+newtype Polynomial a = Polynomial { getTerms :: [Term a] }
+
+instance (Ord a, Num a) => Num (Polynomial a) where
+  (Polynomial p1) + (Polynomial p2) = Polynomial $ combineTerms (p1 <> p2)
+  
+  (Polynomial p1) * (Polynomial p2) = Polynomial $ do
+    Term coeff1 exps1 <- p1
+    Term coeff2 exps2 <- p2
+    -- Sum up exponents of same variables.
+    return $ Term (coeff1*coeff2) $ M.unionWith (+) exps1 exps2
+
+  abs (Polynomial p) = Polynomial (modifyCoeff abs <$> p)
+
+  negate (Polynomial p) = Polynomial (modifyCoeff negate <$> p)
+
+  signum (Polynomial p) = Polynomial (modifyCoeff signum <$> p)
+
+  fromInteger i = Polynomial [Term (fromInteger i) M.empty]
+  
+-- Combine terms with same monomial, e.g. combine 3*x*y and 2*y*x to 5*x*y.
+--
+--  1.  Sort the terms BY the monomials, so equal monomials are next to each other. 
+--
+--  2.  `go` over the terms and sum up equal monomials.
+--
+--  3.  Some terms now might have coefficient `0`. Filter them out.
+--
+combineTerms :: forall a. (Num a, Ord a) => [Term a] -> [Term a]
+combineTerms = filter ((/= 0) . getCoeff) . go . L.sortBy (compare `on` getMonomial)
+  where
+    go :: [Term a] -> [Term a]
+    go (Term coeff1 monomial1 : Term coeff2 monomial2 : terms)
+      | monomial1 == monomial2 = go (Term (coeff1+coeff2) monomial1 : terms)
+      | otherwise = Term coeff1 monomial1 : go (Term coeff2 monomial2 : terms)
+    go terms = terms
+
+--- TODO: make polynomial instance of Num
+-- newtype Polynomial' a = Polynomial' { getTerms :: [Term a] }
+-- instance Num a => Num (Polynomial' a) where
+--   p1 + p2 = j
+
+
 derivative :: Polynomial a -> Polynomial a
 derivative = undefined -- TODO
--- derivative = Polynomial . tail . zipWith (*) [0..] . getCoefficients
 
 -- | Bring expression into normal form as a polynomial: 
 -- 
@@ -32,9 +76,11 @@ derivative = undefined -- TODO
 --   =  3*x^2 + 6*x*y + 3*y^2 + 2*y + 15*1
 -- 
 -- TODO: property test "expression is equivalent to polynomial"
+--       property test "all coeffitients are always non-zero"
+--       property test "key set of all monomials is the same"
 --
 fromExpr :: forall a. (Ord a, Num a) => Expr a -> Polynomial a
-fromExpr = sum_coeffs . expand
+fromExpr = expand
   where
     -- Multiply out nested expressions and eliminate subtractions until
     -- we are left with a big sum, where each summand is composed of a 
@@ -44,13 +90,13 @@ fromExpr = sum_coeffs . expand
     expand :: Expr a -> Polynomial a
     expand expr =
       case expr of
-        Var var -> [ Term 1 [(var, 1)] ]
-        Const a -> [ Term a [] ]
+        Var var -> Polynomial [ Term 1 (M.singleton var 1) ]
+        Const a -> Polynomial [ Term a M.empty ]
 
         UnaryOp (Root _) _ -> error "Roots in user provided expressions not supported"
 
-        UnaryOp (Exp n) (Const a) -> [ Term (a^n) [] ]
-        UnaryOp (Exp n) (Var var) -> [ Term 1 [(var, n)] ]
+        UnaryOp (Exp n) (Const a) -> Polynomial [ Term (a^n) M.empty ]
+        UnaryOp (Exp n) (Var var) -> Polynomial [ Term 1 (M.singleton var n) ]
         UnaryOp (Exp n) expr -> 
           if n < 1 then 
             error "Non-positive exponents not supported"
@@ -59,65 +105,61 @@ fromExpr = sum_coeffs . expand
           else 
             expand $ BinaryOp Mul expr (UnaryOp (Exp (n-1)) expr)
 
-        BinaryOp Add expr1 expr2 -> 
-          expand expr1 <> expand expr2
-
-        BinaryOp Sub expr1 expr2 -> 
-          expand expr1 <> (modifyCoeff negate <$> expand expr2)
-
+        BinaryOp Add expr1 expr2 -> expand expr1 + expand expr2
+        BinaryOp Sub expr1 expr2 -> expand expr1 - expand expr2
+        BinaryOp Mul expr1 expr2 -> expand expr1 * expand expr2
         BinaryOp Div _ _ -> error "Division in user provided expressions not supported"
 
-        BinaryOp Mul expr1 expr2 -> do
-          Term coeff1 vars1 <- expand expr1
-          Term coeff2 vars2 <- expand expr2
-          return $ Term (coeff1*coeff2) $ sum_exponents (vars1 <> vars2)
+    -- -- Make sure each monomial has default entry of zero for each 
+    -- -- variable that is in use in the polynomial.        
+    -- fill :: Polynomial a -> Polynomial a
+    -- fill polynomial = polynomial'
+    --   where
+    --     zero = M.fromSet (const 0) (varsIn polynomial)
+    --     polynomial' =  modifyMonomial (<> zero) <$> polynomial
 
-    -- Sum up exponents of same variables.
-    sum_exponents :: Monomial -> Monomial
-    sum_exponents = go . L.sort
-      where
-        go :: Monomial -> Monomial
-        go ((var1, exp1) : (var2, exp2) : factors)
-          | var1 == var2 = (var1, exp1+exp2) : go factors
-          | otherwise = (var1, exp1) : go ((var2, exp2) : factors)
-        go (factor : factors) = factor : go factors
-        go [] = []
-
-    -- Combine terms with same monomial, e.g. combine 3*x*y and 2*y*x to 5*x*y.
-    --
-    --  1.  Sort each monomial, so for example x*y is recognized as being the 
-    --      same monomial as y*x. 
-    --
-    --  2.  Sort the terms BY the monomials, so equal monomials are next to 
-    --      each other. 
-    --
-    --  3.  `go` over the terms and sum up equal monomials.
-    --
-    --  4.  Some terms now might have coefficient `0`. Filter them out.
-    --
-    sum_coeffs :: Polynomial a -> Polynomial a
-    sum_coeffs = filter ((/= 0) . getCoeff) . go . L.sortBy (compare `on` getMonomial) . fmap (modifyMonomial L.sort)
-      where
-        go :: [Term a] -> [Term a]
-        go ((Term coeff1 monomial1) : (Term coeff2 monomial2) : terms)
-          | monomial1 == monomial2 = go (Term (coeff1+coeff2) monomial1 : terms)
-          | otherwise = Term coeff1 monomial1 : go (Term coeff2 monomial2 : terms)
-        go terms = terms
-
-toExpr :: Polynomial a -> Expr a
-toExpr = foldr1 (BinaryOp Add) . fmap from_term
+toExpr :: forall a. (Ord a, Num a) => Polynomial a -> Expr a
+toExpr = foldr1 (BinaryOp Add) . fmap from_term . getTerms
   where
     from_term :: Term a -> Expr a
     from_term (Term coeff monomial) = 
-      foldr (BinaryOp Mul) (Const coeff) (from_var <$> monomial)
+      foldr (BinaryOp Mul) (Const coeff) (M.mapWithKey from_var monomial)
 
-    from_var :: (Var, Int) -> Expr a
-    from_var (var, n) 
+    from_var :: Var -> Int -> Expr a
+    from_var var n 
       | n == 1 = Var var
       | n > 1  = UnaryOp (Exp n) (Var var)
       | otherwise = error "Non-positive exponents not supported."
 
+-- |
+-- A monomial is linear if all exponents are 0, e.g. `x^0 * y^0 * z^0` = 1.
+-- Or exactly one of the exponents is 1, e.g. x^0 * y^0 * z^1 = z.
 isLinear :: Monomial -> Bool
-isLinear [] = True
-isLinear [(var, 1)] = True
-isLinear _ = False
+isLinear monomial = is_zero || is_unit
+  where 
+    non_zero_exponents = filter (/= 0) $ M.elems monomial
+
+    is_zero = null non_zero_exponents
+    is_unit = [1] == non_zero_exponents
+    
+degree :: (Num a, Ord a) => Polynomial a -> Int
+degree = maximum . fmap (sum . getMonomial) . getTerms
+
+eval :: forall a. Num a => IntMap a -> Polynomial a -> a
+eval assignment polynomial = sum (eval_term <$> getTerms polynomial)
+  where
+    eval_term :: Term a -> a
+    eval_term (Term coeff monomial) = coeff * eval_monomial monomial
+    
+    eval_monomial :: Monomial -> a
+    eval_monomial monomial = value
+      where
+        vars = S.toList $ M.keysSet assignment <> M.keysSet monomial
+    
+        get_base var = M.findWithDefault 1 var assignment
+        get_exp var = M.findWithDefault 0 var monomial
+    
+        value = product [ get_base var ^ get_exp var | var <- vars ]
+
+varsIn :: Polynomial a -> S.IntSet
+varsIn = S.unions . fmap (M.keysSet . getMonomial) . getTerms
--- src/Theory/NonLinearRealArithmatic/Subtropical.hs
+++ src/Theory/NonLinearRealArithmatic/Subtropical.hs
@@ -1,1 +1,97 @@
 module Theory.NonLinearRealArithmatic.Subtropical where
+
+import qualified Theory.NonLinearRealArithmatic.Expr as Expr
+import qualified Data.List as L
+import qualified Data.IntMap as M
+import Data.IntMap ( IntMap )
+import Theory.NonLinearRealArithmatic.Polynomial
+
+-- |
+-- The frame of a polynomial is a set of points, obtained from the 
+-- exponents of the individual monomials. E.g. for a polynomial over 
+-- variables x,y like 
+-- 
+--   y + 2xy^3 - 3x^2y^2 - x^3 - 4x^4y^4
+-- 
+-- we get the following points 
+--
+--   (0,1), (1,3), (2,2), (4,4) 
+--
+-- The points are then partitioned by the sign of the coefficient.
+--
+--   pos: (0,1) 
+--   neg: (1,3), (2,2), (4,4) 
+-- 
+-- Computing the frame is the basis for identifiying a term that
+-- dominates the polynomial for sufficently large variables values.
+-- That in turn is sufficient to find solutions to inequality 
+-- constraints.
+frame :: (Ord a, Num a) => Polynomial a -> ([Monomial], [Monomial])
+frame polynomial = undefined -- TODO
+  where
+    (pos_terms, neg_terms) 
+      = L.partition ((> 0) . getCoeff) 
+      $ L.filter ((/= 0) . getCoeff) (getTerms polynomial)
+
+findDominatingDirection :: (Num a, Ord a) => Polynomial a -> Maybe (IntMap Int)
+findDominatingDirection terms = undefined
+  where
+    pos_terms = filter ((> 0) . getCoeff) (getTerms terms)
+    
+-- |
+-- Returns True iff the polynomial evaluates to something positive under 
+-- the given variable assignment.
+isPositiveSolution :: (Num a, Ord a) => Polynomial a -> IntMap a -> Bool
+isPositiveSolution polynomial solution = eval solution polynomial > 0
+
+-- |
+positiveSolution :: (Num a, Ord a) => Polynomial a -> Maybe (IntMap a)
+positiveSolution polynomial = do 
+  normal_vector <- findDominatingDirection polynomial
+  
+  -- For a sufficiently large base the polynomial should evaluate 
+  -- to something positive.
+  let bases = [ 2^n | n <- [1..] ]
+  let candidates = [ M.map (b^) normal_vector | b <- bases ]      
+
+  L.find (isPositiveSolution polynomial) candidates
+  
+-- newtype Solution a = Sol { getValues :: IntMap a }
+
+-- instance Num a => Num (Solution a) where
+--   (Sol s1) + (Sol s2) = Sol $ M.unionWith (+) s1 s2
+--   (Sol s1) * (Sol s2) = Sol $ M.unionWith (*) s1 s2
+--   abs (Sol s1) = Sol $ M.map abs s1
+--   negate (Sol s1) = Sol $ M.map negate s1
+--   signum (Sol s1) = Sol $ M.map signum s1
+--   fromInteger i = error "TODO: define this"
+  
+-- |
+-- Returns True if the polynomial evaluates to 0 under the given 
+-- variable assignment.
+isSolution :: (Num a, Ord a) => Polynomial a -> IntMap a -> Bool
+isSolution polynomial solution = eval solution polynomial == 0
+  
+-- | 
+findSolution :: forall a. (Num a, Ord a) => Polynomial a -> Maybe (IntMap a)
+findSolution polynomial
+  | eval one polynomial < 0 = go one polynomial
+  | eval one polynomial > 0 = go one (negate polynomial)
+  | otherwise = Just one
+  where
+    -- solution that maps all variables to one
+    one = M.fromSet (const 1) (varsIn polynomial)
+    
+    go :: IntMap a -> Polynomial a -> Maybe (IntMap a)
+    go neg_sol polynomial = do
+      pos_sol <- positiveSolution polynomial
+      
+      -- TODO: solve for t element [0;1]
+      -- neg_sol + t * (pos_sol - neg_sol)
+      let t = 1
+      
+      return 
+        $ M.unionWith (+) neg_sol 
+        $ M.map (* t) 
+        $ M.unionWith (-) pos_sol neg_sol
+      
